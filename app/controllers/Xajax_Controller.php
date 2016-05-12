06a3ebd47dbca210b5e05690a78032b7134980af
feuzeu <thierry.feuzeu@gmail.com>
2016-05-12 01:35:41 -0400

    Renamed the Test controller.

--- app/controllers/Xajax_Controller.php
+++ app/controllers/Xajax_Controller.php
@@ -7,29 +7,23 @@ use Xajax\Xajax;
 
 class Xajax_Controller extends CI_Controller
 {
-    protected $xajax = null;
+	protected $xajax = null;
 
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	public function __construct()
+	{
+		parent::__construct();
+		// Setup the Xajax library
+		$this->xajax = $this->load->library('xajax');
+		$this->xajax->setup();
+	}
 
-    // Default method of the CI controllers
-    protected function setupXajax()
-    {
-        $this->xajax = $this->load->library('xajax');
-        $this->xajax->setup();
-    }
-
-    // Default method of the CI controllers
-    public function index()
-    {
-        // Setup the Xajax library
-        $this->setupXajax();
-    	// Process the Xajax request
-        if($this->xajax->canProcessRequest())
-        {
-            $this->xajax->processRequest();
-        }
-    }
+	// Default method of the CI controllers
+	public function index()
+	{
+		// Process the Xajax request
+		if($this->xajax->canProcessRequest())
+		{
+			$this->xajax->processRequest();
+		}
+	}
 }
