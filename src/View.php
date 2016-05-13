db232450e9392641929af91cb5ade698b4029f89
feuzeu <thierry.feuzeu@gmail.com>
2016-05-12 23:40:09 -0400

    Added the View.php file.

---
+++ src/View.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Xajax\Laravel;
+
+use Illuminate\Pagination\LengthAwarePaginator as Paginator;
+use App;
+
+class View
+{
+	protected static $data;
+	protected $controller = null;
+	protected $ci;
+
+	public function __construct($controller)
+	{
+		self::$data = array();
+		$this->controller = $controller;
+		$this->ci = &get_instance();
+	}
+
+	/**
+	 * Make a piece of data available for all views
+	 *
+	 * @param string		$name			The data name
+	 * @param string		$value			The data value
+	 * 
+	 * @return void
+	 */
+	public function share($name, $value)
+	{
+		self::$data[$name] = $value;
+	}
+
+	/**
+	 * Render a template
+	 *
+	 * @param string		$template		The template path
+	 * @param string		$data			The template data
+	 * 
+	 * @return mixed		The rendered template
+	 */
+	public function render($template, array $data = array())
+	{
+		return $this->ci->load->view($template, array_merge(self::$data, $data), true);
+	}
+}
