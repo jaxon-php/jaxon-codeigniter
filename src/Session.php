7bd09588c00de59053cb7b86763e0731d34067a6
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2017-06-27 18:59:02 +0000

    Apply fixes from StyleCI

--- src/Session.php
+++ src/Session.php
@@ -6,7 +6,7 @@ class Session
 {
     /**
      * The CakePHP session
-     * 
+     *
      * @var object
      */
     protected $xSession = null;
@@ -18,7 +18,7 @@ class Session
 
     /**
      * Get the current session id
-     * 
+     *
      * @return string           The session id
      */
     public function getId()
@@ -28,9 +28,9 @@ class Session
 
     /**
      * Generate a new session id
-     * 
+     *
      * @param bool          $bDeleteData         Whether to delete data from the previous session
-     * 
+     *
      * @return void
      */
     public function newId($bDeleteData = false)
@@ -43,7 +43,7 @@ class Session
      *
      * @param string        $sKey                The session key
      * @param string        $xValue              The session value
-     * 
+     *
      * @return void
      */
     public function set($sKey, $xValue)
@@ -55,7 +55,7 @@ class Session
      * Check if a session key exists
      *
      * @param string        $sKey                The session key
-     * 
+     *
      * @return bool             True if the session key exists, else false
      */
     public function has($sKey)
@@ -68,7 +68,7 @@ class Session
      *
      * @param string        $sKey                The session key
      * @param string        $xDefault            The default value
-     * 
+     *
      * @return mixed|$xDefault             The data under the session key, or the $xDefault parameter
      */
     public function get($sKey, $xDefault = null)
@@ -78,7 +78,7 @@ class Session
 
     /**
      * Get all data in the session
-     * 
+     *
      * @return array             An array of all data in the session
      */
     public function all()
@@ -90,7 +90,7 @@ class Session
      * Delete a session key and its data
      *
      * @param string        $sKey                The session key
-     * 
+     *
      * @return void
      */
     public function delete($sKey)
@@ -100,7 +100,7 @@ class Session
 
     /**
      * Delete all data in the session
-     * 
+     *
      * @return void
      */
     public function clear()
