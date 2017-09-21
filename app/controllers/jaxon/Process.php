3cd14d464cdd00418f1eaf34bf8eb5ef4b72799f
feuzeu <thierry.feuzeu@gmail.com>
2017-09-21 18:25:00 -0400

    Added processing callbacks to the request processing controller.

--- app/controllers/jaxon/Process.php
+++ app/controllers/jaxon/Process.php
@@ -3,6 +3,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Process extends CI_Controller
 {
+    /**
+     * Load the Jaxon and Session libraries.
+     */
     public function __construct()
     {
         parent::__construct();
@@ -12,8 +15,63 @@ class Process extends CI_Controller
         $this->load->library('session');
     }
 
+    /**
+     * Callback for initializing a Jaxon class instance.
+     * 
+     * This function is called anytime a Jaxon class is instanciated.
+     *
+     * @param object            $instance               The Jaxon class instance
+     *
+     * @return void
+     */
+    public function initInstance($instance)
+    {
+    }
+
+    /**
+     * Callback before processing a Jaxon request.
+     *
+     * @param object            $instance               The Jaxon class instance to call
+     * @param string            $method                 The Jaxon class method to call
+     * @param boolean           $bEndRequest            Whether to end the request or not
+     *
+     * @return void
+     */
+    public function beforeRequest($instance, $method, &$bEndRequest)
+    {
+    }
+
+    /**
+     * Callback after processing a Jaxon request.
+     *
+     * @param object            $instance               The Jaxon class instance called
+     * @param string            $method                 The Jaxon class method called
+     *
+     * @return void
+     */
+    public function afterRequest($instance, $method)
+    {
+    }
+
+    /**
+     * Process a Jaxon request.
+     * 
+     * The HTTP response is automatically sent back to the browser
+     *
+     * @return void
+     */
     public function index()
     {
+        $this->jaxon->onInit(function($instance){
+            $this->initInstance($instance);
+        });
+        $this->jaxon->onBefore(function($instance, $method, &$bEndRequest){
+            $this->beforeRequest($instance, $method, $bEndRequest);
+        });
+        $this->jaxon->onAfter(function($instance, $method){
+            $this->afterRequest($instance, $method);
+        });
+
         // Process the Jaxon request
         if($this->jaxon->canProcessRequest())
         {
